fee49ebbb4e5ad45fd04382cc02a4e4629366e01
Aryan Yadav <aryan.yadv57@gmail.com>
2025-01-31 12:32:42 +0530

    Update test2.py

--- TUTORIAL2/test2.py
+++ TUTORIAL2/test2.py
@@ -1,9 +1,3 @@
-# Input two numbers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-# Calculate sum and average
-sum = num1 + num2
-average = sum / 2
-# Display results
-print(f"Sum of the two numbers is: {sum}")
-print(f"Average of the two numbers is: {average}")
+def multiply(x, y): # x,y are parameter
+    return x * y
+print(multiply(2, 3)) # 2,3 are arguments
