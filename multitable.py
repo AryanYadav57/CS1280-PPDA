b4070039a78b9bc39fd3ec327a2d66404f77a210
Aryan Yadav <aryan.yadv57@gmail.com>
2025-02-19 14:52:40 +0530

    Add files via upload

---
+++ multitable.py
@@ -0,0 +1,9 @@
+
+# Get user input
+num = int(input("Enter a number:"))
+
+# Print the multiplication table
+print(f"Multiplication table of {num}:")
+for i in range(1, 11):  # Loop from 1 to 10
+    print(f"{num} x {i} = {num * i}")
+
